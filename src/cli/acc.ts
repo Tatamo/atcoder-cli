160c3aa841bc73c5f1920e7ed4a61e904f2eb94a
Tatamo <nsorder@gmail.com>
2018-10-14 17:59:33 +0900

    add subcommands (implement not yet)

--- src/cli/acc.ts
+++ src/cli/acc.ts
@@ -7,6 +7,15 @@ async function login() {
 	console.log(await session.login());
 }
 
+async function session() {
+	const session = new Session();
+	console.log(await session.check());
+}
+
+async function tasks() {
+	// do nothing
+}
+
 commander
 	.version(version, "-v, --version");
 
@@ -14,5 +23,15 @@ commander
 	.command("login")
 	.action(async () => await login())
 	.description("login to AtCoder");
+
+commander
+	.command("session")
+	.action(async () => await session())
+	.description("check login or not");
+
+commander
+	.command("tasks")
+	.action(async () => await tasks())
+	.description("get tasks");
 
 commander.parse(process.argv);
