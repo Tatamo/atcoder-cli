652ad88ebd38eb23a599ce775692c5071793e838
Tatamo <nsorder@gmail.com>
2018-10-18 16:37:14 +0900

    add OnlineJudge.getPath()

--- src/facade/oj.ts
+++ src/facade/oj.ts
@@ -5,7 +5,7 @@ import {promisify} from "util";
 const exec = promisify(child_process.exec);
 
 export class OnlineJudge {
-	static async checkAvailable() {
+	static async getPath(): Promise<string | null> {
 		const config = getConfig();
 		let path = config.get("oj-path");
 		// configにpathが設定されていない場合はwhichコマンドで探してみる
@@ -13,9 +13,15 @@ export class OnlineJudge {
 			const command = process.platform === "win32" ? "where oj" : "which oj";
 			path = (await exec(command).then(v => v.stdout.trim()).catch(() => ""));
 		}
+		return path === "" ? null : path;
+	}
 
-		if (path === "") return false;
-		const result = await exec(`${path} -h`).then(v => v.stdout).catch(() => "");
-		return result.trim() !== "";
+	static async checkAvailable() {
+		const path = OnlineJudge.getPath();
+		if (path === null) return false;
+		const result = (await exec(`${path} -h`).then(v => v.stdout).catch(() => "")).trim() !== "";
+		// うまくpathが通っていた場合はconfigに登録する
+		if (result && getConfig().get("oj-path") === undefined) getConfig().set("oj-path", path);
+		return result;
 	}
 }
