ba4417e1ffed660b6325348f90ab1543968eda8c
Tatamo <nsorder@gmail.com>
2018-10-20 19:26:15 +0900

    make getConfig() async

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,14 @@
-import Conf from "conf";
+type Conf = import("conf");
 
 /**
  * 常に単一のConfインスタンスを返す関数
  */
-const getConfig: () => Conf = (() => {
+const getConfig: () => Promise<Conf> = (() => {
 	// confはsingletonとして扱う
 	let conf: Conf | null = null;
-	return () => {
+	return async () => {
 		if (conf !== null) return conf;
+		const Conf = (await import("conf")).default;
 		return conf = new Conf();
 	}
 })();
