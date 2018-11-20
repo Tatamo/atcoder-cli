23bfaa4bb09ab8cc4e4280574d8010816155eb46
Tatamo <nsorder@gmail.com>
2018-11-21 02:10:14 +0900

    validate template json

--- src/template.ts
+++ src/template.ts
@@ -23,12 +23,30 @@ export async function getTemplates(path: string): Promise<Array<{ name: string, 
 		try {
 			// ファイル名をディレクトリ名とみなして直下のJSONファイルを取得
 			const template = JSON.parse(await promisify(readFile)(resolve(path, name, TEMPLATE_JSON_FILE_NAME), "utf8"));
-			// TODO: validate
-			templates.push({name, template});
+			const [valid, error] = await validateTemplateJSON(template);
+			if (valid) {
+				templates.push({name, template});
+			}
+			else {
+				console.error(`validation error in ${resolve(name, TEMPLATE_JSON_FILE_NAME)}:\n  ${error}`);
+			}
 		} catch (e) {
 			// ディレクトリでないファイルなど、ENOTDIR, ENOENTエラーが発生した場合はそのまま無視
-			if (e.code !== "ENOTDIR" && e.code !== "ENOENT") throw e;
+			if (e.code !== "ENOTDIR" && e.code !== "ENOENT") console.error(`Error occurred in ${resolve(name, TEMPLATE_JSON_FILE_NAME)}:\n  ${e.toString()}`);
 		}
 	}
 	return templates;
+}
+
+/**
+ * プロジェクトファイルが正しい形式に沿っているか調べる
+ * [valid:true, error:null] | [valid:false, error:string] valid=trueなら正しいJSON
+ * @param data
+ */
+export async function validateTemplateJSON(data: Template): Promise<[true, null] | [false, string]> {
+	const ajv = new ((await import("ajv")).default)();
+	const validate = ajv.compile((await import("./schema")).template_schema);
+	const valid = validate(data);
+	if (!valid) return [false, ajv.errorsText(validate.errors)];
+	return [true, null];
 }
