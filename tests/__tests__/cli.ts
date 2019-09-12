4a0866540d9d458052b18ae6b06f6165b15861a3
Tatamo <nsorder@gmail.com>
2019-09-13 01:53:01 +0900

    add cli tests for new and add commands

--- tests/__tests__/cli.ts
+++ tests/__tests__/cli.ts
@@ -12,7 +12,7 @@ const run = (...args: Array<string>) => {
 
 // コマンドラインからの呼び出しに対して、想定通りの関数が想定通りの引数で呼ばれていることをテストする
 describe("command calls", () => {
-	beforeEach(()=>{
+	beforeEach(() => {
 		// jest.resetModules()でcommandsも毎回別物になるので、jest.mockで毎回mockしなおす
 		// そのためjest.clearAllMocks()は呼ばなくてもよい
 		jest.mock("../../src/commands");
@@ -22,25 +22,66 @@ describe("command calls", () => {
 		jest.resetModules();
 	});
 	describe("acc new", () => {
-		test("new <contest-id>", () => {
+		test("new abc100", () => {
 			const commands = require("../../src/commands");
 			run("new", "abc100");
 			expect(commands.setup).toBeCalledWith("abc100", expect.anything());
+			expect(commands.setup).not.toBeCalledWith("abc101", expect.anything());
 		});
-		test("new <contest-id> (2)", () => {
+		test("n abc102 -c next -f", () => {
 			const commands = require("../../src/commands");
-			run("new", "abc101");
-			expect(commands.setup).toBeCalledWith("abc101", expect.anything());
-			expect(commands.setup).not.toBeCalledWith("abc100", expect.anything());
+			run("n", "abc102", "-c", "next", "-f");
+			expect(commands.setup).toBeCalledWith("abc102", expect.objectContaining({tests: true, force: true, choice: "next"}));
+			expect(commands.setup).toBeCalledWith("abc102", expect.not.objectContaining({template: expect.anything()}));
+		});
+		test("new abc103 --no-tests --no-template", () => {
+			const commands = require("../../src/commands");
+			run("new", "abc103", "--no-tests", "--no-template");
+			expect(commands.setup).toBeCalledWith("abc103", expect.objectContaining({tests: false, template: false}));
+			expect(commands.setup).toBeCalledWith("abc103", expect.not.objectContaining({choice: expect.anything()}));
+		});
+		test("new abc104 -t {TASKLABEL}", () => {
+			const commands = require("../../src/commands");
+			run("new", "abc104", "-t", "{TASKLABEL}");
+			expect(commands.setup).toBeCalledWith("abc104", expect.objectContaining({taskDirnameFormat: "{TASKLABEL}"}));
+		});
+		test("new abc105 -d {ContestTitle}", () => {
+			const commands = require("../../src/commands");
+			run("new", "abc105", "-d", "{ContestTitle}");
+			expect(commands.setup).toBeCalledWith("abc105", expect.objectContaining({contestDirnameFormat: "{ContestTitle}"}));
+		});
+	});
+	describe("acc add", () => {
+		test("add", () => {
+			const commands = require("../../src/commands");
+			run("add");
+			expect(commands.add).toBeCalledWith(expect.anything());
+		});
+		test("a -c next -f", () => {
+			const commands = require("../../src/commands");
+			run("a", "-c", "next", "-f");
+			expect(commands.add).toBeCalledWith(expect.objectContaining({tests: true, force: true, choice: "next"}));
+			expect(commands.add).toBeCalledWith(expect.not.objectContaining({template: expect.anything()}));
+		});
+		test("add -t {TASKLABEL}", () => {
+			const commands = require("../../src/commands");
+			run("a", "-t", "{TASKLABEL}");
+			expect(commands.add).toBeCalledWith(expect.objectContaining({taskDirnameFormat: "{TASKLABEL}"}));
+		});
+		test("add --no-tests --no-template", () => {
+			const commands = require("../../src/commands");
+			run("add", "--no-tests", "--no-template");
+			expect(commands.add).toBeCalledWith(expect.objectContaining({tests: false, template: false}));
+			expect(commands.add).toBeCalledWith(expect.not.objectContaining({choice: expect.anything()}));
 		});
 	});
 	describe("acc tasks", () => {
-		test("tasks [contest-id]", ()=> {
+		test("tasks [contest-id]", () => {
 			const commands = require("../../src/commands");
 			run("tasks", "abc101");
 			expect(commands.tasks).toBeCalledWith("abc101", expect.not.objectContaining({id: true}));
 		});
-		test("tasks -i [contest-id]", ()=> {
+		test("tasks -i [contest-id]", () => {
 			const commands = require("../../src/commands");
 			run("tasks", "-i", "abc101");
 			expect(commands.tasks).toBeCalledWith("abc101", expect.objectContaining({id: true}));
