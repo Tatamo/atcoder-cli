18e2d9186551f825cf9b5dc55839e457b89132fc
Tatamo <nsorder@gmail.com>
2019-09-29 16:04:41 +0900

    add more transaction tests and a removeSession() test

--- tests/__tests__/session.ts
+++ tests/__tests__/session.ts
@@ -2,7 +2,6 @@ import {SessionDesign} from "../../src/di/index";
 import {Cookie} from "../../src/cookie";
 import {disableCookieFileIO} from "../utils";
 import axios from "axios";
-import {SessionInterface} from "../../src/session";
 import {AtCoder} from "../../src/atcoder";
 
 // mock axios
@@ -10,7 +9,7 @@ jest.mock("axios");
 // axios.createで作成されるインスタンスもモックに差し替える
 axios.create = jest.fn(() => axios);
 
-const getTestSession = async (): Promise<SessionInterface> => {
+const getTestSession = async () => {
 	const {container: {session}} = await SessionDesign
 		.bind('CookieConstructor', () => {
 			disableCookieFileIO();
@@ -20,8 +19,8 @@ const getTestSession = async (): Promise<SessionInterface> => {
 	return session;
 };
 
-describe("session", () => {
-	beforeAll(()=>{
+describe("Session", () => {
+	beforeAll(() => {
 		// @ts-ignore: dynamically added method for test
 		axios.mockResolvedValue({
 			status: 200,
@@ -63,18 +62,83 @@ describe("session", () => {
 		const result = await session.post(AtCoder.login_url);
 		expect(result).toMatchSnapshot();
 	});
-	test("transaction", async () => {
-		const session = await getTestSession();
-		await session.transaction(async ()=>{
-			await (await session.get(AtCoder.getContestURL("aic987"))).saveSession();
-			await (await session.get(AtCoder.getTaskURL("aic987"))).saveSession();
-			await (await session.post(AtCoder.login_url)).saveSession();
-			// saveSession()を複数回呼び出しても、Cookieのファイルへの保存は行われない
+	describe("transaction", () => {
+		// transactionが正常に終了するまではCookieがファイルに保存されない
+		test("nop", async () => {
+			const session = await getTestSession();
+			await session.transaction(async () => {
+			});
 			// @ts-ignore: dynamically added property for test
 			expect(Cookie.prototype.saveConfigFile.mock.calls.length).toBe(0);
+			expect((await session.getCookies()).get()).toEqual([]);
 		});
-		// transaction終了時に一度だけsaveConfigFile()が呼び出される
+		test("saveSession", async () => {
+			const session = await getTestSession();
+			await session.transaction(async () => {
+				await (await session.get(AtCoder.getContestURL("aic987"))).saveSession();
+				await (await session.get(AtCoder.getTaskURL("aic987"))).saveSession();
+				await (await session.post(AtCoder.login_url)).saveSession();
+				// saveSession()を複数回呼び出しても、Cookieのファイルへの保存は行われない
+				// @ts-ignore: dynamically added property for test
+				expect(Cookie.prototype.saveConfigFile.mock.calls.length).toBe(0);
+			});
+			// transaction終了時に一度だけsaveConfigFile()が呼び出される
+			// @ts-ignore: dynamically added property for test
+			expect(Cookie.prototype.saveConfigFile.mock.calls.length).toBe(1);
+			expect((await session.getCookies()).get()).toMatchSnapshot();
+		});
+		test("removeSession", async () => {
+			const session = await getTestSession();
+			await session.transaction(async () => {
+				await (await session.post(AtCoder.login_url)).saveSession();
+				await session.removeSession();
+				// @ts-ignore: dynamically added property for test
+				expect(Cookie.prototype.saveConfigFile.mock.calls.length).toBe(0);
+			});
+			// @ts-ignore: dynamically added property for test
+			expect(Cookie.prototype.saveConfigFile.mock.calls.length).toBe(1);
+			expect((await session.getCookies()).get()).toEqual([]);
+		});
+		test("transaction in transaction", async () => {
+			const session = await getTestSession();
+			await expect(
+				session.transaction(async () => {
+					await session.transaction(async () => {
+						await (await session.post(AtCoder.login_url)).saveSession();
+					})
+				})
+			).rejects.toThrow();
+		});
+		test("throw", async () => {
+			const session = await getTestSession();
+			await (await session.post(AtCoder.login_url)).saveSession();
+			expect((await session.getCookies()).get()).toMatchSnapshot();
+			const before = (await session.getCookies()).get();
+			try {
+				await session.transaction(async () => {
+					await (await session.get(AtCoder.getContestURL("aic987"))).saveSession();
+					throw new Error("");
+				})
+			} catch (e) {
+				// do nothing
+			}
+			// transaction中にエラーが発生するとCookieの更新は巻き戻される
+			expect((await session.getCookies()).get()).toEqual(before);
+		})
+	});
+	test("removeSession", async () => {
+		const session = await getTestSession();
+		const result = await session.post(AtCoder.login_url);
+		// @ts-ignore: dynamically added property for test
+		expect(Cookie.prototype.saveConfigFile.mock.calls.length).toBe(0);
+		await result.saveSession();
+		expect((await session.getCookies()).get()).toMatchSnapshot();
 		// @ts-ignore: dynamically added property for test
 		expect(Cookie.prototype.saveConfigFile.mock.calls.length).toBe(1);
-	})
+		// removeSession()の呼び出しにより、保存されたCookieが破棄される
+		await session.removeSession();
+		expect((await session.getCookies()).get()).toEqual([]);
+		// @ts-ignore: dynamically added property for test
+		expect(Cookie.prototype.saveConfigFile.mock.calls.length).toBe(2);
+	});
 });
