bf1863ec7102dc379e7d2c857eb80b4bc2210807
Tatamo <nsorder@gmail.com>
2019-10-23 16:23:24 +0900

    install mock-fs to test getTemplate()

--- tests/__tests__/template.ts
+++ tests/__tests__/template.ts
@@ -1,28 +1,31 @@
 import * as template from "../../src/template";
 import {getConfigDirectory} from "../../src/config";
-import * as fs from "fs";
-
-jest.mock("fs");
+import mock from "mock-fs";
 jest.mock("../../src/config");
+
+const mock_template = {
+	contest: {
+		static: [["gitignore", ".gitignore"]]
+	},
+	task: {
+		submit: "main.cpp",
+		program: ["main.cpp"]
+	}
+};
+const mock_template_json = JSON.stringify(mock_template);
+
+const DUMMY_CONFIG_DIRECTORY_PATH = "/config/dir/of/acc";
 
 describe("template", () => {
 	beforeAll(async () => {
 		// 常に同じpathを返すようにしておく
 		// @ts-ignore: dynamically added method for test
-		getConfigDirectory.mockResolvedValue("/config/dir/of/acc");
+		getConfigDirectory.mockResolvedValue(DUMMY_CONFIG_DIRECTORY_PATH);
 
 	});
 	describe("validateTemplateJSON", () => {
 		test("valid", async () => {
-			const [valid, error] = await template.validateTemplateJSON({
-				contest: {
-					static: [["gitignore", ".gitignore"]]
-				},
-				task: {
-					submit: "main.cpp",
-					program: ["main.cpp"]
-				}
-			});
+			const [valid, error] = await template.validateTemplateJSON(JSON.parse(mock_template_json));
 			expect(valid).toBe(true);
 			expect(error).toBe(null);
 		});
@@ -35,7 +38,12 @@ describe("template", () => {
 		});
 	});
 	test("getTemplate", async () => {
-		// not implemented
-		// TODO: mock fs to write test
+		mock({
+			[`${DUMMY_CONFIG_DIRECTORY_PATH}/template01`]:{
+				"template.json" : mock_template_json
+			}
+		});
+		expect(await template.getTemplate("template01")).toEqual({name:"template01", ...mock_template});
+		mock.restore();
 	});
 });
