3a452028082ef65138d71c13688d8bf89d30145d
Tatamo <nsorder@gmail.com>
2021-08-21 00:16:09 +0900

    add tests for commands.test() and commands.tests()

--- tests/__tests__/commands.ts
+++ tests/__tests__/commands.ts
@@ -1,7 +1,7 @@
-import { login, logout, session, contest } from "../../src/commands";
+import { login, logout, session, contest, task, tasks } from "../../src/commands";
 import { AtCoder } from "../../src/atcoder";
 import { getAtCoder } from "../../src/di";
-import { Contest, ContestProject, findProjectJSON, Task } from "../../src/project"
+import { Contest, ContestProject, findProjectJSON, Task, detectTaskByPath } from "../../src/project"
 jest.mock("../../src/di");
 jest.mock("../../src/project")
 
@@ -152,6 +152,30 @@ describe("Commands", () => {
                 expect(atcoder.contest).toBeCalledTimes(1);
                 expect(atcoder.contest).toBeCalledWith("aic987");
             });
+            test("contest found, try login and success", async () => {
+                atcoder.checkSession = jest.fn(async () => false);
+                atcoder.login = jest.fn(async () => true);
+                atcoder.contest = jest.fn(async _ => dummy_contest);
+                await contest("aic987", {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(findProjectJSON).toBeCalledTimes(0);
+                expect(atcoder.login).toBeCalledTimes(1);
+                expect(atcoder.contest).toBeCalledTimes(1);
+                expect(atcoder.contest).toBeCalledWith("aic987");
+            });
+            test("try login and fail, but (public) contest found", async () => {
+                atcoder.checkSession = jest.fn(async () => false);
+                atcoder.login = jest.fn(async () => false);
+                atcoder.contest = jest.fn(async _ => dummy_contest);
+                await contest("aic987", {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(findProjectJSON).toBeCalledTimes(0);
+                expect(atcoder.login).toBeCalledTimes(1);
+                expect(atcoder.contest).toBeCalledTimes(1);
+                expect(atcoder.contest).toBeCalledWith("aic987");
+            });
             test("contest not found", async () => {
                 atcoder.checkSession = jest.fn(async () => true);
                 atcoder.contest = jest.fn(async _ => {
@@ -163,6 +187,187 @@ describe("Commands", () => {
                 expect(findProjectJSON).toBeCalledTimes(0);
                 expect(atcoder.contest).toBeCalledTimes(1);
                 expect(atcoder.contest).toBeCalledWith("aic987654321");
+            });
+        });
+    });
+    describe("task()", () => {
+        describe("contest id and task id are not given", () => {
+            test("task found, without id", async () => {
+                // @ts-ignore: dynamically added method for test
+                detectTaskByPath.mockResolvedValueOnce({ contest: dummy_contest, task: dummy_task01 });
+                await task(undefined, undefined, {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(detectTaskByPath).toBeCalledTimes(1);
+            })
+            test("task found, with id", async () => {
+                // @ts-ignore: dynamically added method for test
+                detectTaskByPath.mockResolvedValueOnce({ contest: dummy_contest, task: dummy_task01 });
+                await task(undefined, undefined, { id: true });
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(detectTaskByPath).toBeCalledTimes(1);
+            })
+            test("task not found", async () => {
+                // @ts-ignore: dynamically added method for test
+                detectTaskByPath.mockResolvedValueOnce({ contest: null, task: null });
+                await task(undefined, undefined, {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(detectTaskByPath).toBeCalledTimes(1);
+            });
+            test("error occured in searching path", async () => {
+                // TODO: this path seems to be never reached
+                // @ts-ignore: dynamically added method for test
+                detectTaskByPath.mockRejectedValueOnce(new Error("something wrong"));
+                await task(undefined, undefined, {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(detectTaskByPath).toBeCalledTimes(1);
+            });
+        });
+        describe("contest id and task id are given", () => {
+            test("task found, without id", async () => {
+                atcoder.checkSession = jest.fn(async () => true);
+                atcoder.task = jest.fn(async (contest_id, task_id) => dummy_task01);
+                await task("aic987", "aic987_a", {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(detectTaskByPath).toBeCalledTimes(0);
+                expect(atcoder.task).toBeCalledTimes(1);
+                expect(atcoder.task).toBeCalledWith("aic987", "aic987_a");
+            });
+            test("task found, with id", async () => {
+                atcoder.checkSession = jest.fn(async () => true);
+                atcoder.task = jest.fn(async (contest_id, task_id) => dummy_task01);
+                await task("aic987", "aic987_a", { id: true });
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(detectTaskByPath).toBeCalledTimes(0);
+                expect(atcoder.task).toBeCalledTimes(1);
+                expect(atcoder.task).toBeCalledWith("aic987", "aic987_a");
+            });
+            test("contest found, try login and success", async () => {
+                atcoder.checkSession = jest.fn(async () => false);
+                atcoder.login = jest.fn(async () => true);
+                atcoder.task = jest.fn(async (contest_id, task_id) => dummy_task01);
+                await task("aic987", "aic987_a", {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(detectTaskByPath).toBeCalledTimes(0);
+                expect(atcoder.login).toBeCalledTimes(1);
+                expect(atcoder.task).toBeCalledTimes(1);
+                expect(atcoder.task).toBeCalledWith("aic987", "aic987_a");
+            });
+            test("try login and fail, but (public) task found", async () => {
+                atcoder.checkSession = jest.fn(async () => false);
+                atcoder.login = jest.fn(async () => false);
+                atcoder.task = jest.fn(async (contest_id, task_id) => dummy_task01);
+                await task("aic987", "aic987_a", {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(detectTaskByPath).toBeCalledTimes(0);
+                expect(atcoder.login).toBeCalledTimes(1);
+                expect(atcoder.task).toBeCalledTimes(1);
+                expect(atcoder.task).toBeCalledWith("aic987", "aic987_a");
+            });
+            test("contest not found", async () => {
+                atcoder.checkSession = jest.fn(async () => true);
+                atcoder.task = jest.fn(async _ => {
+                    throw new Error();
+                });
+                await task("aic987", "aic987_z", {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(detectTaskByPath).toBeCalledTimes(0);
+                expect(atcoder.task).toBeCalledTimes(1);
+                expect(atcoder.task).toBeCalledWith("aic987", "aic987_z");
+            });
+        });
+    });
+    describe("tasks()", () => {
+        describe("contest id and task id are not given", () => {
+            test("tasks found, without id", async () => {
+                // @ts-ignore: dynamically added method for test
+                findProjectJSON.mockResolvedValueOnce({ path: "/dummy/path", data: dummy_contest_project });
+                await tasks(undefined, {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(findProjectJSON).toBeCalledTimes(1);
+            })
+            test("tasks found, with id", async () => {
+                // @ts-ignore: dynamically added method for test
+                findProjectJSON.mockResolvedValueOnce({ path: "/dummy/path", data: dummy_contest_project });
+                await tasks(undefined, { id: true });
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(findProjectJSON).toBeCalledTimes(1);
+            })
+            test("tasks not found", async () => {
+                // @ts-ignore: dynamically added method for test
+                findProjectJSON.mockRejectedValueOnce(new Error("contest.acc.json not found."));
+                await tasks(undefined, {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(findProjectJSON).toBeCalledTimes(1);
+            });
+        });
+        describe("contest id and task id are given", () => {
+            test("tasks found, without id", async () => {
+                atcoder.checkSession = jest.fn(async () => true);
+                atcoder.tasks = jest.fn(async (contest_id) => [dummy_task01, dummy_task02, dummy_task03, dummy_task04]);
+                await tasks("aic987", {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(findProjectJSON).toBeCalledTimes(0);
+                expect(atcoder.tasks).toBeCalledTimes(1);
+                expect(atcoder.tasks).toBeCalledWith("aic987");
+            });
+            test("task found, with id", async () => {
+                atcoder.checkSession = jest.fn(async () => true);
+                atcoder.tasks = jest.fn(async (contest_id) => [dummy_task01, dummy_task02, dummy_task03, dummy_task04]);
+                await tasks("aic987", { id: true });
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(findProjectJSON).toBeCalledTimes(0);
+                expect(atcoder.tasks).toBeCalledTimes(1);
+                expect(atcoder.tasks).toBeCalledWith("aic987");
+            });
+            test("contest found, try login and success", async () => {
+                atcoder.checkSession = jest.fn(async () => false);
+                atcoder.login = jest.fn(async () => true);
+                atcoder.tasks = jest.fn(async (contest_id) => [dummy_task01, dummy_task02, dummy_task03, dummy_task04]);
+                await tasks("aic987", {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(findProjectJSON).toBeCalledTimes(0);
+                expect(atcoder.login).toBeCalledTimes(1);
+                expect(atcoder.tasks).toBeCalledTimes(1);
+                expect(atcoder.tasks).toBeCalledWith("aic987");
+            });
+            test("try login and fail, but (public) task found", async () => {
+                atcoder.checkSession = jest.fn(async () => false);
+                atcoder.login = jest.fn(async () => false);
+                atcoder.tasks = jest.fn(async (contest_id) => [dummy_task01, dummy_task02, dummy_task03, dummy_task04]);
+                await tasks("aic987", {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(findProjectJSON).toBeCalledTimes(0);
+                expect(atcoder.login).toBeCalledTimes(1);
+                expect(atcoder.tasks).toBeCalledTimes(1);
+                expect(atcoder.tasks).toBeCalledWith("aic987");
+            });
+            test("contest not found", async () => {
+                atcoder.checkSession = jest.fn(async () => true);
+                atcoder.tasks = jest.fn(async _ => {
+                    throw new Error();
+                });
+                await tasks("aic987", {});
+                expect(log).toMatchSnapshot();
+                expect(elog).toMatchSnapshot();
+                expect(findProjectJSON).toBeCalledTimes(0);
+                expect(atcoder.tasks).toBeCalledTimes(1);
+                expect(atcoder.tasks).toBeCalledWith("aic987");
             });
         });
     });
